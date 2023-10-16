146416a962d0dff4e0cafb19efc19b8e17acd7ad
samanta-bordallo <bordallo.samanta@gmail.com>
2023-10-16 12:40:49 +0100

    versão com tarefa de notas ponderadas

--- tarefa_para_git.cpp
+++ tarefa_para_git.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
-#include <iomanip>
-#include <math.h>
-
 using namespace std;
 
-int main(void){
-	float a, b, c, d;
-	cout<<"Me de 4 notas"<<endl;
-	cin>>a>>b>>c>>d;
-	cout<<"As notas foram: "<<a<<", "<<b<<", "<<c<<", "<<d<<"."<<endl;
-	cout<<"A media das notas e: "<<(a+b+c+d)/4;
+int main() {
+    float a, b, c, d;
+    cout << "Me dê 4 notas: " << endl;
+    cin >> a >> b >> c >> d;
+    cout << "As notas foram: " << a << ", " << b << ", " << c << ", " << d << "." << endl;
+
+    float mediaPonderada = (a * 0.5 + b * 0.1 + c * 0.1 + d * 0.3) / 4.0;
+
+    cout << "A média das notas ponderadas é: " << mediaPonderada << endl;
+
+    return 0;
 }
+
